9ca262970df5f08b729d2bea1d630db5a7741da8
pesterev da <pesterev_da@skbkontur.ru>
2016-04-09 14:45:33 +0500

    Log gin ggg

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -1,4 +1,5 @@
-﻿using Nancy.Simple.Models;
+﻿using System;
+using Nancy.Simple.Models;
 using Newtonsoft.Json.Linq;
 
 namespace Nancy.Simple
@@ -13,17 +14,25 @@ namespace Nancy.Simple
 		public static int BetRequest(JObject gameState)
 		{
 		    var state = gameState.ToObject<GameState>();
-
 		    var me = state.Players[state.InAction];
 
-		    if (desider.GetPositionGoodness(me, state) > 0.5)
+		    Console.WriteLine("Step {0}-{1}, money {2}", state.Round, state.BetIndex, me.Stack);
+
+		    var positionGoodness = desider.GetPositionGoodness(me, state);
+		    Console.WriteLine("Goodness {0}", positionGoodness);
+		    if (positionGoodness > 0.5)
 		    {
-		        return performer.Raise(state, me.Stack/2);
+		        var toAdd = performer.Raise(state, me.Stack/2);
+                Console.WriteLine("Raise - adding {0}", toAdd);
+		        return toAdd;
 		    }
 
 		    var needToCall = performer.Call(state);
 
-		    return needToCall < 0.1 * me.Stack ? performer.Call(state) : performer.Check(state);
+		    var letsCall = needToCall < 0.1 * me.Stack;
+		    var toAdd2 = letsCall ? performer.Call(state) : performer.Check(state);
+		    Console.WriteLine("Letscall={0} adding {1}", letsCall, toAdd2);
+		    return toAdd2;
 		}
 
 		public static void ShowDown(JObject gameState)
