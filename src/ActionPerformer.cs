b948acb1b888507d057a5531c20e29f28737296d
kvadros <kvadros@skbkontur.ru>
2016-04-09 14:24:33 +0500

    Not always AllIn and Check.

---
+++ src/ActionPerformer.cs
@@ -0,0 +1,32 @@
+﻿using Nancy.Simple.Models;
+
+namespace Nancy.Simple
+{
+    public class ActionPerformer
+    {
+        public int Call(GameState state)
+        {
+            return state.MinimumRaise;
+        }
+
+        public int Check(GameState state)
+        {
+            return 0;
+        }
+
+        public int Fold(GameState state)
+        {
+            return 0;
+        }
+
+        public int Raise(GameState state, int amount)
+        {
+            return Call(state) + amount;
+        }
+
+        public int AllIn(GameState state)
+        {
+            return int.MaxValue;
+        }
+    }
+}
--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -1,22 +1,29 @@
-﻿using System.Linq;
-using Nancy.Simple.Models;
+﻿using Nancy.Simple.Models;
 using Newtonsoft.Json.Linq;
 
 namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Air v1.0.1";
+		public static readonly string VERSION = "Air v1.0.2";
 
         private static PositionDesider desider = new PositionDesider();
+        private static ActionPerformer performer = new ActionPerformer();
 
 		public static int BetRequest(JObject gameState)
 		{
 		    var state = gameState.ToObject<GameState>();
+
 		    var me = state.Players[state.InAction];
-		    return desider.GetPositionGoodness(me, state) > 0.5
-                ? int.MaxValue
-                : 0;
+
+		    if (desider.GetPositionGoodness(me, state) > 0.5)
+		    {
+		        return performer.Raise(state, me.Stack/2);
+		    }
+
+		    var needToCall = performer.Call(state);
+
+		    return needToCall < 0.1 * me.Stack ? performer.Call(state) : performer.Check(state);
 		}
 
 		public static void ShowDown(JObject gameState)
