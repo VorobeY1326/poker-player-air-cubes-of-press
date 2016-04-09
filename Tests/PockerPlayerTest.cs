e8451c6d72d9c61c8be34b86d5d29ab2c6c29253
pesterev da <pesterev_da@skbkontur.ru>
2016-04-09 13:56:41 +0500

    v1.0.1

---
+++ Tests/PockerPlayerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nancy.Simple;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class PockerPlayerTest
+    {
+        [Test]
+        public void RunBetR()
+        {
+            Console.WriteLine(PokerPlayer.BetRequest(null));
+        }
+    }
+}
---
+++ src/PositionDesider.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Nancy.Simple.Models;
+
+namespace Nancy.Simple
+{
+    public class PositionDesider
+    {
+        //returns from 0 to 1
+        public double GetPositionGoodness(Player me, GameState state)
+        {
+            var cardA = me.HoleCards[0];
+            var cardB = me.HoleCards[0];
+            var ok = new[] {"8", "9", "10", "J", "Q", "K", "A"};
+            if (cardA.Rank == cardB.Rank && ok.Contains(cardA.Rank))
+                return 1;
+            return 0;
+        }
+    }
+}
