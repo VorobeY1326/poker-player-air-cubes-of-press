1c7ca406e4d39de6e4c1e533b38c43e4ba6cd0bf
pesterev da <pesterev_da@skbkontur.ru>
2016-04-09 13:06:33 +0500

    tests

---
+++ Tests/Parsing.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Parsing
+    {
+        [Test]
+        public void Test()
+        {
+            
+        }
+    }
+}
